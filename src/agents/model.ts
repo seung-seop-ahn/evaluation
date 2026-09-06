ca0313f2a24286083f445854c52cc0d3f88c28e9
seung-seop-ahn <seungseopahn@gmail.com>
2026-09-06 20:02:38 +1200

    fix: model

--- src/agents/model.ts
+++ src/agents/model.ts
@@ -1,7 +1,7 @@
 import { ChatOpenAI } from '@langchain/openai';
 
 export const model = new ChatOpenAI({
-    model: 'gpt-4',
+    model: 'gpt-5',
     apiKey: process.env.API_KEY,
     reasoning: {
         effort: 'high',
